10ea17f483b164f1aca244a4b9a17f811860108d
theLeoNoir <lazami.leonor@gmail.com>
2023-08-09 13:32:40 +0200

    changed bg color

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -64,10 +64,7 @@ const Projects = () => {
   ];
 
   return (
-    <div
-      name="projects"
-      className="bg-gradient-to-b from-gray-900 via-gray-900 to-black w-full text-white md:h-screen"
-    >
+    <div name="projects" className="bg-[#0a0908] w-full text-white md:h-screen">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-9 mb-5">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -47,10 +47,7 @@ const Skills = () => {
   ];
 
   return (
-    <div
-      name="skills"
-      className="bg-gradient-to-b from-gray-900 via-gray-900 to-black w-full h-screen"
-    >
+    <div name="skills" className="bg-[#000F1A] w-full h-screen">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
